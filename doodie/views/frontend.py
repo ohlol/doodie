d491301e71bce142e04e4335b298e2fb10b2e251
Scott Smith <scott@boundary.com>
2012-11-20 16:03:49 -0800

    no scrubs

--- doodie/views/frontend.py
+++ doodie/views/frontend.py
@@ -3,8 +3,6 @@ from flask import Module, jsonify, render_template
 from doodie import pagerduty_apikey, pagerduty_subdomain
 from doodie.lib.pagerduty import Pagerduty
 
-import random
-
 frontend = Module(__name__)
 
 @frontend.route("/incidents")
@@ -16,16 +14,6 @@ def quick():
 def incidents_count(status):
     p = Pagerduty(pagerduty_apikey, pagerduty_subdomain)
     count = p.incident_count(status=status)
-    # count = [
-    #     {
-    #         "id": "acknowledged",
-    #         "value": random.randint(1,10)
-    #     },
-    #     {
-    #         "id": "triggered",
-    #         "value": random.randint(1,10)
-    #     }
-    # ]
 
     if count:
           return jsonify(message=count)
